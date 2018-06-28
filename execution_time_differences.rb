b5ff1c9bb42c5146e84eee902f2d3a656a263527
manofsteele <jeremiahsteele@gmail.com>
2018-06-28 16:30:22 -0700

    Crack the contiguous subsum II method

--- execution_time_differences.rb
+++ execution_time_differences.rb
@@ -1,5 +1,7 @@
+require 'byebug'
+
 def my_min1(arr)
-  
+
   arr.each do |el1, idx1|
     least = true
     arr.each_with_index do |el2, idx2|
@@ -8,13 +10,13 @@ def my_min1(arr)
           least = false
         end
       end
-    end 
+    end
     return el1 if least == true
   end
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min1(list) 
+p my_min1(list)
 # 0.172s, n^2
 
 
@@ -27,25 +29,25 @@ def my_min2(arr)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min2(list) 
+p my_min2(list)
 #0.166s, n
 
 
 def largest_contiguous_subsum1(list)
-  sub_arrays = [] 
-  (0...list.length).each do |i| 
+  sub_arrays = []
+  (0...list.length).each do |i|
     sub_arrays << [list[i]]
-    (i + 1...list.length).each do |j| 
+    (i + 1...list.length).each do |j|
       sub_arrays << sub_arrays.last + [list[j]]
     end
-  end 
+  end
   # sub_arrays
-  max_value = sub_arrays.first.first 
-  sub_arrays.each do |sub| 
+  max_value = sub_arrays.first.first
+  sub_arrays.each do |sub|
     max_value = sub.reduce (:+) if sub.reduce(:+) > max_value
-  end 
+  end
   max_value
-end 
+end
 
 
 list = [5, 3, -7]
@@ -61,15 +63,14 @@ p largest_contiguous_subsum1(list) # => -1 (from [-1])
 
 def largest_contiguous_subsum(list)
   totals = []
-  list.each do |el|
-    totals.each do |total| 
-      totals << total + el
-    end
-    totals << el
+  current = list[0]
+  greatest = list[0]
+  list[1..-1].each do |el|
+    current + el < el ? current = el : current += el
+    greatest = current if current > greatest
   end
-   totals.max
+  greatest
 end
-
 
 
 
@@ -81,5 +82,3 @@ p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
 list = [-5, -1, -3]
 p largest_contiguous_subsum(list) # => -1 (from [-1])
-
-
