c81df70e6e67bc62e1d4e06adf3d010ff302f842
manofsteele <jeremiahsteele@gmail.com>
2018-06-28 18:05:48 -0700

    Finish anagrams exercise

--- anagrams.rb
+++ anagrams.rb
@@ -30,5 +30,40 @@ def second_anagram?(str1, str2)
   chars1.empty? && chars2.empty? ? true : false
 end
 
-p second_anagram?("gizmosally", "sallysally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+
+
+def third_anagram?(str1, str2)
+
+  str1.chars.sort == str2.chars.sort
+
+end
+
+p third_anagram?("gizmosally", "sallysally")    #=> false
+p third_anagram?("elvis", "lives")    #=> true
+
+
+def fourth_anagram?(str1, str2)
+  hash1, hash2 = Hash.new(0), Hash.new(0)
+  str1.chars.each {|ch1| hash1[ch1] += 1}
+  str2.chars.each {|ch2| hash2[ch2] += 1}
+
+  hash1 == hash2
+
+end
+
+p fourth_anagram?("gizmosally", "sallysally")    #=> false
+p fourth_anagram?("elvis", "lives")
+
+def fifth_anagram?(str1, str2)
+  hash1 = Hash.new(0)
+  str1.chars.each {|ch1| hash1[ch1] += 1}
+
+  str2.chars.each {|ch2| hash1[ch2] -= 1}
+
+  hash1.values.all? {|el| el == 0}
+
+end
+
+
+p fifth_anagram?("gizmosally", "sallysally")    #=> false
+p fifth_anagram?("elvis", "lives")
